b3a7f0a32053efd26c5832202a4058a05bb02942
sainsaji <sain.saji@mca.christuniversity.in>
2022-08-12 03:14:09 +0530

    threading enabled

--- write_test.py
+++ write_test.py
@@ -1,18 +1,27 @@
-import pygsheets
+import socket
+import time
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 8254
+s.bind(("0.0.0.0", port))
+s.listen(0)
 
-#authorization
-gc = pygsheets.authorize(service_file='./key/key.json')
-sh = gc.open('test_sheet')
-wks = sh[0]
-current_row_count = len(wks.get_all_records()) + 1
-cell_addr = "E"+str(current_row_count)
-print(cell_addr)
-value = wks.get_value(cell_addr)
+hostname=socket.gethostname()   
+IPAddr=socket.gethostbyname(hostname)   
+print("Your Computer Name is:"+hostname)   
+print("Your Computer IP Address is:"+IPAddr) 
+print(f"Port: {port}")
 
-if len(value)==0:
-    wks.update_value(cell_addr, "test", parse=None)
-
-
-
-
+while True:
+    client, addr = s.accept()
+    client.settimeout(5)
+    while True:
+        content = client.recv(10000)
+        if len(content) == 0:
+            break
+        if str(content, "utf-8") == "\r\n":
+            continue
+        else:
+            print(str(content, "utf-8"))
+            client.send(b"Hello From Python")
+    client.close()
